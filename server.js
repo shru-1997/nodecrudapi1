71d7f03c4638378b70e05cd52272fc62177775a3
Shruthi Reddy <shruthi.reddy@qentelli.com>
2022-01-27 17:52:27 +0530

    get change

--- server.js
+++ server.js
@@ -17,23 +17,44 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Database Connection String
 const connectionString ="mongodb+srv://shruthireddy:shruthi1709@cluster0.6lloq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-// Connecting the database
+// Connecting the database server
 MongoClient.connect(connectionString,{useUnifiedTopology:true})
  .then(client => {
      console.log('connected to database server')
+     // Connecting the database
      const db= client.db('star-wars-quotes')
+     // Connecting the collection
      const quotesCollection = db.collection('quotes')
      
+     // 1. Create with POST
      // Two parameters first one route, second one is function what you want to execute
         app.post('/quotes', (req,res) => {
+            // Inserting the document
             quotesCollection.insertOne(req.body)
+            // Post inserting getting the result
             .then(result=>{
-                console.log(result)
+                res.send(result)
             })
+            // Error for document
             .catch(error=>console.error(error))
         })
 
-}).catch(console.error)
+    // 2. Reading data from MongoDB
+        app.get('/getall',(req,res)=>{
+            // Finding the collection quotes and changing object of objects to array of objects
+             db.collection('quotes').find().toArray()
+             // Waiting for the promise to send us the result back
+            .then(result=>{
+              res.send(result)
+            })
+            // Waiting for the promise to send us the error back
+            .catch(error=>console.error(error))
+
+        })
+
+}).catch(console.error) // MongoDB Atlas Cluster/Server Connection Error
+
+
 
 
 
