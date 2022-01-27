da0394b0cca3a99f00b1ac8cc3110b4e9a501b02
Shruthi Reddy <shruthi.reddy@qentelli.com>
2022-01-27 13:28:27 +0530

    database connection

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const express = require('express')
 // Body Parser for urlencoded form data 
 const bodyParser = require('body-parser')
 
+// Importing Mongo client
+const MongoClient = require('mongodb').MongoClient 
+
 // Creating app function from the express functional constructor to use it for creating
 // server and apis
 const app = express()
 
 // Enabling body parser with urlencoded form data to be true
 app.use(bodyParser.urlencoded({extended:true}))
+
+// Database Connection String
+const connectionString ="mongodb+srv://shruthireddy:shruthi1709@cluster0.6lloq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+// Connecting the database
+MongoClient.connect(connectionString,(err,client)=>{
+    if(err) return console.error(err)
+    console.log('connected to database')
+})
 
 // Two parameters first one route, second one is function what you want to execute
 app.post('/quotes', (req,res) => {
